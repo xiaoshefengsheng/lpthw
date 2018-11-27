db9ec3397102f75a37db8d95920e69f0ef0a5690
xiaoshefengsheng <chenruining@qq.com>
2018-11-27 22:26:52 +0800

    20181127create

---
+++ ex32.py
@@ -0,0 +1,17 @@
+# #a是一个列表，然后x是计算列表元素的长度，y初始为0
+# a =('php', 'nginx', 'tomcat')
+# x = len(a)
+# y = 0
+# #执行循环，当y在x的整数列表中，则输出a[y]+"=yes",且赋值给b，打印b,不换行. 然后y+1
+# for y in range(x):
+    # b = (a[y]+"=yes")
+    # print(b, end = ' ')
+    # y += 1
+
+
+the_count = [1, 2, 3, 4, 5]
+fruits = ['apples', 'oranges', 'pears', 'apricots']
+change = [1, 'pennies', 2, 'dimes', 3, 'quarters']
+
+# this first kind of for-loop goes through a list
+for number in the_count:	
